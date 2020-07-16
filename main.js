d2e7b53f1bedfed8e82f6b8bd698e16eaaf5e686
youngrongoh <youngrong.oh@gmail.com>
2020-07-16 10:33:43 +0900

    improve after watch a lesson video

--- main.js
+++ main.js
@@ -3,27 +3,18 @@ const plus = document.querySelector('.plusBtn');
 const inputText = document.querySelector('.inputText');
 
 // Add item
+let id = 0;
 function createItem(text) {
     // Create a item element as li tag
     const item = document.createElement('li'); // <li></li>
     item.setAttribute('class', 'item'); // <li class="item"></li>
-
-    // Create a item's name element as div tag
-    const name = document.createElement('div'); // <div></div>
-    name.setAttribute('class', 'item__name'); // <div class="item__name"></div>
-    name.textContent = text; // <div class="item__name" data-name="text">text</div>
-
-    // Create a delete button adding icon
-    const deleteBtn = document.createElement('button'); // <button></button>
-    deleteBtn.setAttribute('class', 'item__deleteBtn'); // <button class="item__deleteBtn"></button>
-    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>'; // <button class="item__deleteBtn"><i class="fas fa-trash-alt"></i></button>
-
-    // Add child elements into item
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-
-    // Add a item into shopping list
-    list.appendChild(item);
+    item.setAttribute('data-id', id);
+    item.innerHTML = `
+        <div class="item__name" data-name="text">${text}</div>
+        <button class="item__delete">
+            <i class="fas fa-trash-alt" data-id=${id}></i>
+        </button>`;
+    id++;
 }
 
 function onAdd() {
@@ -33,18 +24,11 @@ function onAdd() {
         return;
     }
     createItem(text);
+    list.appendChild(item);
+    // Reset input tap
     inputText.value = '';
     inputText.focus();
 }
-
-
-list.addEventListener('click', (e) => {
-    if (e.target.tagName !== 'I') {
-        return;
-    }
-    const thisItem = e.target.parentElement.parentElement;
-    list.removeChild(thisItem);
-});
 
 // Add item when a plus button is clicked.
 plus.addEventListener('click', () => {
@@ -55,5 +39,14 @@ plus.addEventListener('click', () => {
 inputText.addEventListener('keypress', (e) => {
     if (e.key == 'Enter') {
         onAdd();
+    }
+});
+
+//  remove item when clicked a delete button in it.
+list.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+        const toBeDeleted = document.querySelector(`.item[data-id="${id}"]`)
+        toBeDeleted.remove();
     }
 });
